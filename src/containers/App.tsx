20d3d57f4e8b18799d244c088d1a8e774c015cf0
Thibaud LARIVIERE <thibaud.lariviere@effidence.com>
2022-01-12 11:02:38 +0100

    Add footer

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import Header from "../components/Header";
 import CV from "./CV";
+import Footer from "../components/Footer";
 import styles from "./App.module.scss";
 
 const App: React.FC = () => {
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <div className={styles.App}>
         <Header />
         <CV />
+        <Footer />
       </div>
     </BrowserRouter>
   );
